a98ce7efa3343751b16701f9d55bebd14f65f71e
hongduck710 <hongik710@gmail.com>
2025-03-15 20:33:56 +0900

    7 - Error Exception Handling 까지 완료

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,24 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+//@desc Register a new user
+//@ropute /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+    //validation
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields!')
+    }
+    res.send('Register Route')
+})
+
+//@desc Login a user
+//@ropute /api/users/login
+//@access Public
+const loginUser = asyncHandler(async (req, res) => {
     res.send('login Route')
-}
+})
 
 module.exports = {
     registerUser,
